090190a2290a14453bd84da2ccf2348c34faa011
tkpy543 <takspooh@gmail.com>
2022-04-29 01:36:30 +0900

    最短経路を取得するエンドポイントの追加

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import Fastify from 'fastify'
 import 'dotenv/config'
 import { validatePort } from './validatePort'
+import { dijkstra } from './dijkstra/main'
 
 const port =
   validatePort(process.env.PORT) ??
@@ -15,6 +16,17 @@ fastify.get('/', async (request, reply) => {
   if (userAgent === undefined)
     throw new Error(`statusCode: 400, message: "user-agent is empty"`)
   return userAgent
+})
+
+fastify.get<{
+  QueryString: { shiten: string; shuten: string }
+  Headers: any
+}>('/get_saitan_keiro', async (request, reply) => {
+  const { shiten, shuten } = request.query as { shiten: string; shuten: string }
+  return { shiten, shuten }
+  // try {
+  //   return dijkstra(shiten, shuten)
+  // } catch (error) {}
 })
 
 fastify.listen({ port }).catch((err) => {
